9556fa1f0f7a7403620aed754cd22f199507c9af
Dave Kindel <dkindel@vt.edu>
2014-12-04 10:05:52 -0500

    forgot to add CHM in last commit

---
+++ Flat Combining Hash Map/src/finalproj/CHM/CHM.java
@@ -0,0 +1,38 @@
+package finalproj.CHM;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import finalproj.map.HM;
+
+
+public class CHM<K,V> implements HM<K, V> {
+
+	private ConcurrentHashMap<K,V> map;
+	
+	public CHM(int cap){
+		map = new ConcurrentHashMap<K,V>(cap);
+	}
+	
+	
+	@Override
+	public V put(K key, V value) {
+		return map.put(key, value);
+	}
+
+	@Override
+	public V remove(K key) {
+		return map.remove(key);
+	}
+
+	@Override
+	public V get(K key) {
+		return map.get(key);
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
